c08896d1fb1b9d90f68dc14e7755d5c1599cb524
FelineBuna <felinemabel.buna08@gmail.com>
2017-10-20 01:59:01 +0800

    changes in wsgi

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -6,15 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
 import os
 import sys
-
 path ='/home/FelineBuna/web'
 if path not in sys.path:
     sys.path.append(path)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 from django.core.wsgi import get_wsgi_application
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+
+application = get_wsgi_application()
