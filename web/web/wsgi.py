8c4e52e5083a7e5393bbe4ca844a94d0c6999be9
FelineBuna <felinemabel.buna08@gmail.com>
2017-10-20 12:32:14 +0800

    changed the settings again

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 import os
 import sys
-path ='/home/FelineBuna/web'
+
+path ='/home/pythoanywhereusername/projectname'
 if path not in sys.path:
     sys.path.append(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectname.settings")
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
